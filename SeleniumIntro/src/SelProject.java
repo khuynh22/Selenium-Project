33982ff06e72f22040fcbdc4a076765f452365c4
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-14 15:45:23 -0500

    Run three browser at a time

--- SeleniumIntro/src/SelProject.java
+++ SeleniumIntro/src/SelProject.java
@@ -12,26 +12,36 @@ public class SelProject implements WebDriver {
 		// Safari - SafariDriver -> Methods close
 		// WebDriver close get
 		// WebDriver methods + class methods
-		// chromeddriver.exe -> Chrome browser
-//		System.setProperty("webdriver.chrome.driver",
-//				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
-//		WebDriver driver = new ChromeDriver();
-
-		// Firefox Launch
-		// geckodriver
-
-//		System.setProperty("webdriver.gecko.driver", "/Users/khangnguyenhuynh/Downloads/geckodriver");
-//		WebDriver driver = new FirefoxDriver();
 		
-		// Microsoft Edge
+		
+		// Chrome browser
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
+		WebDriver driver1 = new ChromeDriver();
+		driver1.get("https://www.khang-nguyen-huynh.com/");
+		System.out.println(driver1.getTitle());
+		System.out.println(driver1.getCurrentUrl());
+		driver1.close();
+		driver1.quit();
+		
+		// Microsoft Edge browser
 		System.setProperty("webdriver.gecko.driver", "/Users/khangnguyenhuynh/Desktop/Selenium/msedgedriver");
-		WebDriver driver = new EdgeDriver();
+		WebDriver driver3 = new EdgeDriver();
+		driver3.get("https://www.khang-nguyen-huynh.com/");
+		System.out.println(driver3.getTitle());
+		System.out.println(driver3.getCurrentUrl());
+		driver3.close();
+		driver3.quit();
+		
+		// Firefox browser
+		System.setProperty("webdriver.gecko.driver", "/Users/khangnguyenhuynh/Downloads/geckodriver");
+		WebDriver driver2 = new FirefoxDriver();
+		driver2.get("https://www.khang-nguyen-huynh.com/");
+		System.out.println(driver2.getTitle());
+		System.out.println(driver2.getCurrentUrl());
+		driver2.close();
+		driver2.quit();
 
-		driver.get("https://www.khang-nguyen-huynh.com/");
-		System.out.println(driver.getTitle());
-		System.out.println(driver.getCurrentUrl());
-		driver.close();
-		driver.quit();
 	}
 
 }

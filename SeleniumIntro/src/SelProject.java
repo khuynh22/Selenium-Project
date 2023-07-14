1b26edd0c3ba65297b8415e177c36b6887ebefde
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-14 15:31:18 -0500

    update

--- SeleniumIntro/src/SelProject.java
+++ SeleniumIntro/src/SelProject.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class SelProject implements WebDriver {
 
 	public static void main(String[] args) {
@@ -10,8 +12,16 @@ public class SelProject implements WebDriver {
 		// WebDriver close get
 		// WebDriver methods + class methods
 		// chromeddriver.exe -> Chrome browser
-		System.setProperty("webdriver.chrome.driver", "/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
-		WebDriver driver = new ChromeDriver();
+//		System.setProperty("webdriver.chrome.driver",
+//				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
+//		WebDriver driver = new ChromeDriver();
+
+		// Firefox Launch
+		// geckodriver
+
+		System.setProperty("webdriver.gecko.driver", "/Users/khangnguyenhuynh/Downloads/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+
 		driver.get("https://www.khang-nguyen-huynh.com/");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());

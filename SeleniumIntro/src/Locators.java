ad0b763a556f8ee4ea08ad95d427d83e0e0925d2
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-15 00:20:40 -0500

    class name and css locators

--- SeleniumIntro/src/Locators.java
+++ SeleniumIntro/src/Locators.java
@@ -13,6 +13,7 @@ public class Locators {
 		driver.findElement(By.id("inputUsername")).sendKeys("tim");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello12");
 		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 	}
 
 }

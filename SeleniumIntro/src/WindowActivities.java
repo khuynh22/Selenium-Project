1ef8931a15521b49ca46cfa20e913bd63a5ca75b
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-16 15:23:07 -0500

    add win automation

---
+++ SeleniumIntro/src/WindowActivities.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivities {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("http://google.com");
+		driver.navigate().to("https://rahulshettyacademy.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+
+	}
+
+}

78779bf7842bfe977292a01f6c69e26224ec1ab6
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-17 13:27:49 -0500

    Dropdown looping

--- SeleniumIntro/src/tests/UpdatedDropdown.java
+++ SeleniumIntro/src/tests/UpdatedDropdown.java
@@ -3,6 +3,7 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UpdatedDropdown {
 
@@ -11,17 +12,38 @@ public class UpdatedDropdown {
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://spicejet.com");
+		driver.get("https://spicejet.com");
 		driver.findElement(By.id("divpaxinfo")).click();
-		Thread.sleep(2000);
+
+		   Thread.sleep(2000L);
+
+		   /*int i=1;
+
+		while(i<5)
+
+		{
+
 		driver.findElement(By.id("hrefIncAdt")).click();
-		int i = 1;
-		while (i < 5) {
-			driver.findElement(By.id("hrefIncAdt")).click();
-			i++;
+
+		i++;
+
+		}*/
+
+		   System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+		for(int i=1;i<5;i++)
+
+		{
+
+		driver.findElement(By.id("hrefIncAdt")).click();
+
 		}
-		
+
 		driver.findElement(By.id("btnclosepaxoption")).click();
+
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 	}
 
 }

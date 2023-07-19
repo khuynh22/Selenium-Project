f0acec0f70b6c64b6a71f2842d9a459197fc3c46
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-19 12:40:10 -0500

    alertSystem

---
+++ SeleniumIntro/src/tests/alertTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class alertTest {
+
+	public static void main(String[] args) {
+		String text = "Rahul";
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://spicejet.com"); // URL in the browser
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys(text);
+		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		driver.findElement(By.id("confirmbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+	}
+
+}

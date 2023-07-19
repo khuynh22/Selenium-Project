da97fac60e275495b5527d80babde96c0f23fc4f
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-19 15:20:58 -0500

    Sending Array of Products to Cart for checkout

--- SeleniumIntro/src/ECommerceSelenium/base.java
+++ SeleniumIntro/src/ECommerceSelenium/base.java
@@ -1,6 +1,10 @@
 package ECommerceSelenium;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class base {
@@ -10,6 +14,22 @@ public class base {
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
 		WebDriver driver = new ChromeDriver();
+	
+		//expected checking Array
+		String[] names = {"Cucumber", "Brocolli"};
+		
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+		for (int i = 0; i < products.size(); i++) {
+			String name = products.get(i).getText();
+			if (name.contains("Cucumber")) {
+				// click on "ADD TO CART"
+				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+				break;
+			}
+		}
+
 	}
 
 }

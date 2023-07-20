91b6b445b3854dec13a36055e637adfe0252a0cd
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-20 14:42:41 -0500

    Create frameTest.java

---
+++ SeleniumIntro/src/tests/frameTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class frameTest {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+	}
+
+}

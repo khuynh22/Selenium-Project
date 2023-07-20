291a2e7845b608f8299bcbeb2dd8d06012402c7f
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-20 00:06:39 -0500

    Practical examples on Implicit wait

--- SeleniumIntro/src/ECommerceSelenium/base.java
+++ SeleniumIntro/src/ECommerceSelenium/base.java
@@ -2,6 +2,7 @@ package ECommerceSelenium;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +16,7 @@ public class base {
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
 		// expected checking Array
 		String[] items = { "Cucumber", "Brocolli", "Beetroot", "Carrot" };
@@ -23,7 +25,9 @@ public class base {
 		addItem(driver, items);
 		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[contains(text(), 'PROCEED TO CHECKOUT')]")).click();
-		driver.findElement(By.cssSelector("input.promocode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
 	}
 
 	public static void addItem(WebDriver driver, String[] items) {

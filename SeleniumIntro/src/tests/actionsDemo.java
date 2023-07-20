c25bba1e1537ffb81bf0f95fa87a351921269ad0
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-20 14:20:37 -0500

    Actions class-real time

--- SeleniumIntro/src/tests/actionsDemo.java
+++ SeleniumIntro/src/tests/actionsDemo.java
@@ -1,7 +1,9 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -11,9 +13,13 @@ public class actionsDemo {
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		Actions a = new Actions(driver);
-		a.moveToElement(driver.findElement(By.cssSelector("a[id = 'nav-link-accountList']"))).build().perform();
+		WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello")
+				.build().perform();
+		a.moveToElement(move).contextClick().build().perform();
 	}
 
 }

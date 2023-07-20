b4da8c2c4ad7c8afc247e545eb5ba8531c2c8d48
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-20 12:54:13 -0500

    Handling Ajax/Mouse Interactions

---
+++ SeleniumIntro/src/tests/actionsDemo.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class actionsDemo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		Actions a = new Actions(driver);
+		a.moveToElement(driver.findElement(By.cssSelector("a[id = 'nav-link-accountList']"))).build().perform();
+	}
+
+}

0e056888d240acfd3270d7bcf9583f08c6447c11
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-22 11:52:09 -0500

    Handling HTTPS certifications in Automated browsers

--- SeleniumIntro/src/tests/AutocompleteDropdownAssignment.java
+++ SeleniumIntro/src/tests/AutocompleteDropdownAssignment.java
@@ -3,11 +3,7 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutocompleteDropdownAssignment {
 

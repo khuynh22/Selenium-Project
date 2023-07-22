129d44ef9c7d53ea038a1031a87b2274ed52f859
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-22 01:24:41 -0500

    handle table Grids in webpage

--- SeleniumIntro/src/tests/JavaScriptExecutorDemo.java
+++ SeleniumIntro/src/tests/JavaScriptExecutorDemo.java
@@ -1,7 +1,11 @@
 package tests;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecutorDemo {
@@ -15,6 +19,13 @@ public class JavaScriptExecutorDemo {
 		js.executeScript("window.scrollBy(0,500)");
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+		List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+
+		int sum = 0;
+		for (int i = 0; i < values.size(); i++) {
+			sum += Integer.parseInt(values.get(i).getText());
+		}
+		System.out.println(sum);
 	}
 
 }

1db6360d265c2a1211c5f41db477efa461143f23
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-22 12:12:24 -0500

    Explore Chrome options to set proxies,plugins & paths on Chrome browser

--- SeleniumIntro/src/tests/SSLCheck.java
+++ SeleniumIntro/src/tests/SSLCheck.java
@@ -1,5 +1,9 @@
 package tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,14 +15,19 @@ public class SSLCheck {
 	public static void main(String[] args) {
 		ChromeOptions options = new ChromeOptions();
 		options.setAcceptInsecureCerts(true);
-		
+//		FirefoxOptions options1 = new FirefoxOptions();
+//		options1.setAcceptInsecureCerts(true);
+//
+//		EdgeOptions options2 = new EdgeOptions();
+//		options2.setAcceptInsecureCerts(true);
 
-		FirefoxOptions options1 = new FirefoxOptions();
-		options1.setAcceptInsecureCerts(true);
-		
+		Proxy proxy = new Proxy();
+		proxy.setHttpProxy("ipaddress:4444");
+		options.setCapability("proxy", proxy);
 
-		EdgeOptions options2 = new EdgeOptions();
-		options2.setAcceptInsecureCerts(true);
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("download.default_directory", "/directory/path");
+		options.setExperimentalOption("prefs", prefs);
 
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");

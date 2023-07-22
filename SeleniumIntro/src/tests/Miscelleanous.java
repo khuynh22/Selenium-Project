9fa6ddb681efbf427190dff8daaa1c4dc2884a3f
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-22 12:51:02 -0500

    take Screenshots in Selenium

--- SeleniumIntro/src/tests/Miscelleanous.java
+++ SeleniumIntro/src/tests/Miscelleanous.java
@@ -1,11 +1,17 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
 
 public class Miscelleanous {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
 		WebDriver driver = new ChromeDriver();
@@ -13,6 +19,10 @@ public class Miscelleanous {
 		driver.manage().deleteAllCookies();
 		driver.manage().deleteCookieNamed("sessionKey");
 		driver.get("http://google.com");
+
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File(
+				"/Users/khangnguyenhuynh/Desktop/Selenium/Selenium-Project/SeleniumIntro/src/tests/screenshot.png"));
 	}
 
 }

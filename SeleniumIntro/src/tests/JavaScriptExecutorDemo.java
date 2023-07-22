0ddd272260c43eb330510906c403530368710179
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-22 00:04:57 -0500

    Perform Scrolling with in table and Window level using JavaScriptExecutor

---
+++ SeleniumIntro/src/tests/JavaScriptExecutorDemo.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptExecutorDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver_mac_arm64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(3000);
+		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+	}
+
+}

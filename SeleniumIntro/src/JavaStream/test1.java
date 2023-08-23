46a5c8bb7b288e9253f69ca42efa1240a2b5ea57
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-08-23 16:34:44 -0500

    Stream and sort

--- SeleniumIntro/src/JavaStream/test1.java
+++ SeleniumIntro/src/JavaStream/test1.java
@@ -1,12 +1,14 @@
 package JavaStream;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.testng.annotations.Test;
 
 public class test1 {
-
 	@Test
 	public void regular() {
 		ArrayList<String> names = new ArrayList<String>();
@@ -43,15 +45,21 @@ public class test1 {
 		}).count();
 
 		System.out.println(d);
-		
+
 		// output all the names of ArrayList
 		names.stream().filter(s -> s.length() > 4).forEach(s -> System.out.println(s));
 		names.stream().filter(s -> s.length() > 4).limit(1).forEach(s -> System.out.println(s));
 	}
-	
+
 	@Test
 	public void streamMap() {
-		Stream.of("Adams", "Alvin", "Tim", "Hayley", "Chip").filter(s -> s.endsWith("a")).map(s -> s.toUpperCase())
-		.);
+//		Stream.of("Adams", "Alvin", "Tim", "Hayley", "Chip").filter(s -> s.endsWith("a")).map(s -> s.toUpperCase())
+//				.forEach(s -> System.out.println(s));
+		// print names which have first letter as a with uppercase and sorted
+		
+		List<String> names = Arrays.asList("Adams", "Alvin", "Tim", "Hayley", "Chip");
+		names.stream().filter(s -> s.startsWith("A")).sorted().map(s -> s.toUpperCase()).forEach(s -> System.out.println(s));
+//		names.stream().filter(s -> s.startsWith("A")).sorted().map(s -> s.toUpperCase()).forEach(s -> System.out.println(s));
+		
 	}
 }

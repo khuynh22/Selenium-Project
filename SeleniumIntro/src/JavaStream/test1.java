cce3399ecf1a4000257b027d64d6af2f22786ef7
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-08-24 22:51:54 -0500

    finish stream intro

--- SeleniumIntro/src/JavaStream/test1.java
+++ SeleniumIntro/src/JavaStream/test1.java
@@ -4,8 +4,10 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class test1 {
@@ -56,10 +58,33 @@ public class test1 {
 //		Stream.of("Adams", "Alvin", "Tim", "Hayley", "Chip").filter(s -> s.endsWith("a")).map(s -> s.toUpperCase())
 //				.forEach(s -> System.out.println(s));
 		// print names which have first letter as a with uppercase and sorted
-		
-		List<String> names = Arrays.asList("Adams", "Alvin", "Tim", "Hayley", "Chip");
-		names.stream().filter(s -> s.startsWith("A")).sorted().map(s -> s.toUpperCase()).forEach(s -> System.out.println(s));
-//		names.stream().filter(s -> s.startsWith("A")).sorted().map(s -> s.toUpperCase()).forEach(s -> System.out.println(s));
+
+		ArrayList<String> names = new ArrayList<String>();
+		int count = 0;
+		names.add("Hello");
+		names.add("How");
+		names.add("Are");
+		names.add("You");
+		names.add("Today");
+		List<String> names1 = Arrays.asList("Adams", "Alvin", "Tim", "Hayley", "Chip");
+		names.stream().filter(s -> s.startsWith("A")).sorted().map(s -> s.toUpperCase())
+				.forEach(s -> System.out.println(s));
+		Stream<String> newStream = Stream.concat(names.stream(), names1.stream());
+		newStream.sorted().forEach(s -> System.out.println(s));
+
+		boolean flag = newStream.anyMatch(s -> s.equalsIgnoreCase("Are"));
+		Assert.assertTrue(flag);
+	}
+
+	@Test
+	public void streamCollect() {
+		List<String> ls = Stream.of("Adama", "Alvin", "Tim", "Hayley", "Chip").filter(s -> s.endsWith("a"))
+				.map(s -> s.toUpperCase()).collect(Collectors.toList());
+		System.out.println(ls.get(0));
+		List <Integer> values = Arrays.asList(3,2,2,7,5,1,9,7);
+//		values.stream().distinct().forEach(s -> System.out.println(s));
+		List<Integer> li = values.stream().distinct().sorted().collect(Collectors.toList());
+		System.out.println(li.get(2));
 		
 	}
 }

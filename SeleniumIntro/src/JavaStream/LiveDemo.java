2092cc879976e6fbe5d4375ac19655017d7569ba
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-08-26 10:34:53 -0500

    Update LiveDemo.java

--- SeleniumIntro/src/JavaStream/LiveDemo.java
+++ SeleniumIntro/src/JavaStream/LiveDemo.java
@@ -16,24 +16,25 @@ public class LiveDemo {
 				"/Users/khangnguyenhuynh/Desktop/Selenium/chromedriver-mac-arm64/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
-		//capture all webElements into a list
+		// capture all webElements into a list
 		driver.findElement(By.xpath("//tr/th[1]")).click();
-		
-		//capture all WebElements into list
+
+		// capture all WebElements into list
 		List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
-		
-		//capture text of all WebElements into new (original) list
+
+		// capture text of all WebElements into new (original) list
 		List<String> originalList = elementsList.stream().map(s -> s.getText()).collect(Collectors.toList());
-		
-		//sort on the original list of step 3 -> sorted list
+
+		// sort on the original list of step 3 -> sorted list
 		List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
 		Assert.assertTrue(originalList.equals(sortedList));
-		
-		List<String> price = elementsList.stream().filter(s -> s.getText().contains("Beans")).map(s -> getPriceVeggie(s)).collect(Collectors.toList());
+
+		List<String> price = elementsList.stream().filter(s -> s.getText().contains("Beans"))
+				.map(s -> getPriceVeggie(s)).collect(Collectors.toList());
 		price.forEach(a -> System.out.println(a));
-		
+
 	}
-	
+
 	private static String getPriceVeggie(WebElement s) {
 		String priceValue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
 		return priceValue;
